f81c30f9cc8bdddaadf4769d932e8a80a8bde4d1
mkm777 <kamolbekmkm777@icloud.com>
2025-06-26 13:24:26 +0500

    Kodni yangiladim: foydalanuvchiga yangi habar qo‘shildi

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
-const token = '7612126652:AAFncxHTMoQCAzpKbnyhyQ3sSStLs4YSJro';
-const bot = new TelegramBot(token, { webHook: { port: false } });
+const token = '7252261041:AAH4QH8ZvkZZuDy7KEIRKeMEF2bZK2Tuck4';
+//const bot = new TelegramBot(token, { webHook: { port: false } });
+const bot = new TelegramBot(token);
 const app = express();
 const PORT = process.env.PORT || 10000;
 const WEBHOOK_URL = `https://uztour-auth-server.onrender.com/bot${token}`;
@@ -17,6 +18,7 @@ app.post(`/bot${token}`, (req, res) => {
   bot.processUpdate(req.body);
   res.sendStatus(200);
 });
+
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const opts = {
@@ -26,16 +28,22 @@ bot.onText(/\/start/, (msg) => {
       one_time_keyboard: true
     }
   };
-  bot.sendMessage(chatId, "Iltimos, kontakt ma'lumotlaringizni yuboring:", opts);
+  bot.sendMessage(chatId, "Kontakt malumotingizni yuboring", opts);
+  bot.sendMessage(chatId,"Kanalga azo bo'ling:\nInstagram: https://www.instagram.com/kamolbek_muzaffarov/\nYouTube: https://www.youtube.com/@Kamolbek_Muzaffarov", opts);
 });
+
 bot.on('contact', (msg) => {
   const chatId = msg.chat.id;
   const userId = msg.from.id;
+  if (!msg.contact) {
+    bot.sendMessage(chatId, "Iltimos,kontaktingizni yuboring");
+    return;
+  }
   const contact = msg.contact;
   const currentIndex = userCodes[userId] ?? 0;
   const codeToSend = codes[Math.min(currentIndex, codes.length - 1)];
-  bot.sendMessage(chatId, `✅${codeToSend}✅`);
-  bot.sendMessage(chatId, `Xabaringizni yozishingiz mumkin va men uni Kamolbekga yetkazib qoyaman va birozdan keyin uning ozi shu bot orqali sizga javob  beradi`);
+  bot.sendMessage(chatId, `${codeToSend}`);
+  bot.sendMessage(chatId, "Salom\nQanday yordam bera olaman?");
   userCodes[userId] = currentIndex + 1;
   const contactMessage1 = `👤: ${contact.first_name}\n📱: ${contact.phone_number}\n🆔: ${userId}`;
   bot.sendMessage('5613554119', contactMessage1);
